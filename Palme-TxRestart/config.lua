c127ba1827a945598f62936e202f2fd9c529ec03
Palme <mathiasnielsen2106@gmail.com>
2024-06-21 09:14:13 +0200

    Update config.lua

--- Palme-TxRestart/config.lua
+++ Palme-TxRestart/config.lua
@@ -5,5 +5,7 @@ Config.Settings = {
     ServerName = '888Palme', -- Insert your own server name here
     AutoKick = true, -- Set to true if you want to kick everyone, before the restart
     KickMSG = 'There is a big storm, you have been kicked for your own safety', -- Edit the message players being kicked for
+    SetWind = 1.0,
+    SetWindSpeed = 2.0,
     WeatherType = 'THUNDER', -- Set the current weather, when "secondsRemaining" is being called
 }
